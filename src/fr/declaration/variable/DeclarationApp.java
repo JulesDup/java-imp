f49e50257789d952945220e6ef938fdf131c03d0
Jules Dupuis <jules.dupuis51@gmail.com>
2020-02-28 12:37:26 +0100

    correction configuration

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -1,8 +1,13 @@
 package fr.declaration.variable;
 
+import java.awt.List;
+
 public class DeclarationApp {
 
 	public static void main(String[] args) {
+		List list = new List();  
+
+		
 		byte b = 1;
 		short s = 1;
 		int i = 12;
@@ -11,7 +16,7 @@ public class DeclarationApp {
 		double d = 42;
 		char c = 'c';
 		boolean bou = true;
-
+		
 	}
 
 }
