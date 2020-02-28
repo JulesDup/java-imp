719170eb7e57c26cbd745697fbe352e8d707b0ea
Jules Dupuis <jules.dupuis51@gmail.com>
2020-02-28 12:18:35 +0100

    initialisation projet

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,17 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		byte b = 1;
+		short s = 1;
+		int i = 12;
+		long l = 123L;
+		float f = 44654f;
+		double d = 42;
+		char c = 'c';
+		boolean bou = true;
+
+	}
+
+}
