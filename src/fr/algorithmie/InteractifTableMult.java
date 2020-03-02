112202404f9ded74d3458a3c90c6dac52876b2b6
Jules Dupuis <jules.dupuis51@gmail.com>
2020-03-02 01:57:42 +0100

    InteractifTableMult

---
+++ src/fr/algorithmie/InteractifTableMult.java
@@ -0,0 +1,20 @@
+package fr.algorithmie;
+
+import java.util.Scanner;
+
+public class InteractifTableMult {
+
+	public static void main(String[] args) {
+		Scanner saisie = new Scanner(System.in);
+		int num;
+		do {
+			System.out.println("Choisissez un nombre entre 1 et 10. ");
+			num = saisie.nextInt();
+		} while (num < 1 || num > 10);
+		for (int i = 0; i < 10; i++) {
+			System.out.println(num + " + " + i + " = " + num * i);
+		}
+
+	}
+
+}
